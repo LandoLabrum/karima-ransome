9e35df0c0738ad087e8dbb12dcda894d11d363ec
LandoLabrum <lando@deepturn.com>
2022-01-07 23:29:45 -0700

    Updates

--- main.js
+++ main.js
@@ -36,15 +36,15 @@ $(document).ready(function () {
     }
   };
 
-  var items = document.getElementsByClassName("fade-item");
-  for (let i = 0; i < items.length; ++i) {
-    fadeIn(items[i], i * 1000)
-  }
-  function fadeIn (item, delay) {
-    setTimeout(() => {
-      item.classList.add('fadein')
-    }, delay)
-  }
+  // var items = document.getElementsByClassName("fade-item");
+  // for (let i = 0; i < items.length; ++i) {
+  //   fadeIn(items[i], i * 1000)
+  // }
+  // function fadeIn (item, delay) {
+  //   setTimeout(() => {
+  //     item.classList.add('fadein')
+  //   }, delay)
+  // }
 
   const formatToPhone = (event) => {
     if (isModifierKey(event)) { return; }
